7cd18261ddaacdb8415271d914d8245f5c6665bf
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2023-01-15 01:00:17 -0600

    Sum operations

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 use regex::Regex;
 
 fn main() {
-    println!("Hello, world!");
+    // Regular expresions
+    let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+
+    // User data
+    println!("Please enter the math expression: ");
+    let mut expression = String::new();
+    std::io::stdin().read_line(&mut expression).unwrap();
+
+    // Additions
+    loop {
+        // Apply math operations
+        let caps = re_add.captures(expression.as_str());
+
+        if caps.is_none() {
+            break;
+        }
+
+        let caps = caps.unwrap();
+
+        let cap_expression = caps.get(0).unwrap().as_str();
+        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+        let addition = left_value + right_value;
+
+        expression = expression.replace(cap_expression, &addition.to_string());
+    }
+
+
+    // Show results
+    println!("The result is: {expression}");
 }
